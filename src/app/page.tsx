d79d6a852ed2abdb9ec812662fbbd0f0bc510051
heatyux <421743974@qq.com>
2025-08-11 15:46:15 +0800

    feat: get workspaces api handler created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
 'use client'
 
+import { useEffect, useMemo } from 'react'
+
 import { UserButton } from '@/features/auth/components/user-button'
+import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces'
 
 export default function Home() {
+  const { data, isLoading } = useGetWorkspaces()
+
+  const workspace = useMemo(() => data?.[0]?._id, [data])
+
+  useEffect(() => {
+    if (isLoading) return
+
+    if (workspace) {
+      console.log('Redirect to workspace')
+    } else {
+      console.log('Open create modal')
+    }
+  }, [isLoading, workspace])
+
   return (
     <div>
       <UserButton />
