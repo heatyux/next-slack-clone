2d90a888a189a7f744ac6581bc2bb03be64291a6
heatyux <421743974@qq.com>
2025-08-13 17:17:51 +0800

    feat: update workspace preferences modal implemented

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -3,13 +3,21 @@
 import { useState } from 'react'
 
 import { Trash } from 'lucide-react'
+import { toast } from 'sonner'
 
+import { Button } from '@/components/ui/button'
 import {
   Dialog,
+  DialogClose,
   DialogContent,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { useUpdateWorkspace } from '@/features/workspaces/api/use-update-workspace'
+import { useGetWrokspaceId } from '@/features/workspaces/hooks/use-get-workspace-id'
 
 interface PreferencesModalProps {
   open: boolean
@@ -22,25 +30,89 @@ export const PreferencesModal = ({
   setOpen,
   initialValue,
 }: PreferencesModalProps) => {
+  const workspaceId = useGetWrokspaceId()
   const [value, setValue] = useState(initialValue)
+  const [editOpen, setEditOpen] = useState(false)
+
+  const { mutate: updateWorkspace, isPending } = useUpdateWorkspace()
+
+  const handleEditWrorkspace = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    updateWorkspace(
+      {
+        id: workspaceId,
+        name: value,
+      },
+      {
+        onSuccess: () => {
+          toast.success('Workspace updated.')
+          setEditOpen(false)
+        },
+        onError: () => {
+          toast.error('Failed to update workspace.')
+        },
+      },
+    )
+  }
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open || isPending} onOpenChange={setOpen}>
       <DialogContent className="overflow-hidden bg-gray-50 p-0">
         <DialogHeader className="border-b bg-white p-4">
-          <DialogTitle>{value}</DialogTitle>
+          <DialogTitle>{initialValue}</DialogTitle>
         </DialogHeader>
 
         <div className="flex flex-col gap-y-2 px-4 pb-4">
-          <div className="rounded-lg border bg-white px-5 py-4 hover:bg-gray-50">
-            <div className="flex items-center justify-between">
-              <p className="text-sm font-semibold">Workspace name</p>
-              <button className="cursor-pointer text-sm font-semibold text-[#1264A3] hover:underline">
-                Edit
-              </button>
-            </div>
-            <p className="text-sm">{value}</p>
-          </div>
+          <Dialog open={editOpen || isPending} onOpenChange={setEditOpen}>
+            <DialogTrigger asChild>
+              <div className="flex w-full cursor-pointer flex-col rounded-lg border bg-white px-5 py-4 hover:bg-gray-50">
+                <div className="flex w-full items-center justify-between">
+                  <p className="text-sm font-semibold">Workspace name</p>
+                  <button className="cursor-pointer text-sm font-semibold text-[#1264A3] outline-0 hover:underline">
+                    Edit
+                  </button>
+                </div>
+                <p className="text-sm">{initialValue}</p>
+              </div>
+            </DialogTrigger>
+
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Rename this workspace</DialogTitle>
+              </DialogHeader>
+
+              <form onSubmit={handleEditWrorkspace} className="space-y-4">
+                <Input
+                  disabled={isPending}
+                  value={value}
+                  onChange={(e) => setValue(e.target.value)}
+                  required
+                  autoFocus
+                  minLength={3}
+                  maxLength={20}
+                  placeholder="Workspace name e.g 'Work', 'Personal', 'Home"
+                />
+
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button
+                      disabled={isPending}
+                      variant="outline"
+                      type="button"
+                      onClick={() => setValue(initialValue)}
+                    >
+                      Cancel
+                    </Button>
+                  </DialogClose>
+
+                  <Button disabled={isPending} type="submit">
+                    Save
+                  </Button>
+                </DialogFooter>
+              </form>
+            </DialogContent>
+          </Dialog>
 
           <button className="flex cursor-pointer items-center gap-x-2 rounded-lg border bg-white px-4 py-5 text-rose-600 hover:bg-gray-50">
             <Trash className="size-4" />
