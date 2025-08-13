75fb44fba103fa148b003db8bbb906148f02ac87
heatyux <421743974@qq.com>
2025-08-13 10:36:31 +0800

    feat: workspace layout updated

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -2,6 +2,12 @@
 
 import { PropsWithChildren } from 'react'
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
+
 import { Sidebar } from './sidebar'
 import { Toolbar } from './toolbar'
 
@@ -13,7 +19,22 @@ const WorkspaceIdLayout = ({ children }: PropsWithChildren) => {
       <div className="flex h-[calc(100vh_-_40px)]">
         <Sidebar />
 
-        {children}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId="slack-clone-workspace-layout"
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            workspace sidebar
+          </ResizablePanel>
+
+          <ResizableHandle withHandle />
+
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   )
