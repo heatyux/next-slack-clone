fe9543ac19c956e3d71d80f5820e2d3a7da60a2b
heatyux <421743974@qq.com>
2025-08-14 16:09:20 +0800

    feat: import create channel modal in modal provider

--- src/components/modal-provider.tsx
+++ src/components/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react'
 
+import { CreateChannelModal } from '@/features/channels/components/create-channel-modal'
 import { CreateWorkspaceModal } from '@/features/workspaces/components/create-workspace-modal'
 
 export const ModalProvider = () => {
@@ -18,6 +19,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateWorkspaceModal />
+      <CreateChannelModal />
     </>
   )
 }
