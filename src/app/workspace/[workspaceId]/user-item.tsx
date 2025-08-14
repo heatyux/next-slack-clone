343544a640845bd220dbe0a15aea6e9c717bf904
heatyux <421743974@qq.com>
2025-08-14 14:46:34 +0800

    feat: workspace user item implemented

---
+++ src/app/workspace/[workspaceId]/user-item.tsx
@@ -0,0 +1,61 @@
+import { type VariantProps, cva } from 'class-variance-authority'
+import Link from 'next/link'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { useWorkspaceId } from '@/hooks/use-workspace-id'
+import { cn } from '@/lib/utils'
+
+import type { Id } from '../../../../convex/_generated/dataModel'
+
+const userItemVariants = cva(
+  'flex items-center gap-1.5 justify-start font-normal h-7 px-4 text-sm overflow-hidden',
+  {
+    variants: {
+      variant: {
+        default: 'text-[#F9EDFC]',
+        active: 'text-[#481349] bg-white/90 hover:bg-white/90',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
+interface UserItemProps {
+  id: Id<'members'>
+  label?: string
+  image?: string
+  variant?: VariantProps<typeof userItemVariants>['variant']
+}
+
+export const UserItem = ({
+  id,
+  label = 'Member',
+  image,
+  variant,
+}: UserItemProps) => {
+  const workspaceId = useWorkspaceId()
+  const avatarFallback = label.charAt(0).toUpperCase()
+
+  return (
+    <Button
+      variant="transparent"
+      size="sm"
+      className={cn(userItemVariants({ variant }))}
+      asChild
+    >
+      <Link href={`/workspace/${workspaceId}/member/${id}`}>
+        <Avatar className="mr-1 size-5 rounded-md">
+          <AvatarImage src={image} alt={label} className="rounded-md" />
+          <AvatarFallback className="rounded-md bg-sky-500 text-xs font-semibold text-white">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+
+        <span className="truncate text-sm">{label}</span>
+      </Link>
+    </Button>
+  )
+}
