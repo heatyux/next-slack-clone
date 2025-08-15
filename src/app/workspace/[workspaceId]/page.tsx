95a72b33a582b87d06538d1816d2da33d924155b
heatyux <421743974@qq.com>
2025-08-15 14:35:14 +0800

    feat: workspace id page updated

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,7 +1,75 @@
 'use client'
 
+import { useEffect, useMemo } from 'react'
+
+import { Loader, TriangleAlert } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+import { useGetChannels } from '@/features/channels/api/use-get-channels'
+import { useCreateChannelModal } from '@/features/channels/store/use-create-channel-modal'
+import { useGetWorkspaceInfo } from '@/features/workspaces/api/use-get-workspace-info'
+import { useWorkspaceId } from '@/hooks/use-workspace-id'
+
 const WorkspaceIdPage = () => {
-  return <>Workspace Id Page</>
+  const router = useRouter()
+  const workspaceId = useWorkspaceId()
+  const [open, setOpen] = useCreateChannelModal()
+
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspaceInfo({
+    id: workspaceId,
+  })
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  })
+
+  const channelId = useMemo(() => channels?.[0]?._id, [channels])
+
+  useEffect(() => {
+    if (workspaceLoading || channelsLoading || !workspace) return
+
+    if (channelId) {
+      router.replace(`/workspace/${workspaceId}/channel/${channelId}`)
+    } else if (!open && !workspace.isMember) {
+      setOpen(true)
+    }
+  }, [
+    channelId,
+    channelsLoading,
+    open,
+    router,
+    setOpen,
+    workspace,
+    workspaceId,
+    workspaceLoading,
+  ])
+
+  if (workspaceLoading || channelsLoading) {
+    return (
+      <div className="flex h-full items-center justify-center">
+        <Loader className="size-6 animate-spin" />
+      </div>
+    )
+  }
+
+  if (!workspaceId || !workspace) {
+    return (
+      <div className="felx-col flex h-full flex-1 items-center justify-center gap-2 bg-[#5E2C5F]/95 text-white">
+        <TriangleAlert className="size-6" />
+        <span className="text-sm">Workspace not found.</span>
+      </div>
+    )
+  }
+
+  if (!channels || channels?.length === 0) {
+    return (
+      <div className="felx-col flex h-full flex-1 items-center justify-center gap-2 bg-[#5E2C5F]/95 text-white">
+        <TriangleAlert className="size-6" />
+        <span className="text-sm">No Channels found.</span>
+      </div>
+    )
+  }
+
+  return null
 }
 
 export default WorkspaceIdPage
