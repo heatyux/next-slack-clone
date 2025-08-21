605a119e244ab304e1683bb207c9552eef5523b5
heatyux <421743974@qq.com>
2025-08-21 15:21:40 +0800

    feat: upload image functionality for editor submit implemented

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -7,10 +7,20 @@ import type Quill from 'quill'
 import { toast } from 'sonner'
 
 import { useCreateMessage } from '@/features/messages/api/use-create-message'
+import { useGenerateUploadUrl } from '@/features/upload/api/use-generate-upload-url'
 import { useChannelId } from '@/hooks/use-channel-id'
 import { useWorkspaceId } from '@/hooks/use-workspace-id'
 
+import type { Id } from '../../../../../../convex/_generated/dataModel'
+
 const Editor = dynamic(() => import('@/components/editor'), { ssr: false })
+
+type CreateMessageValues = {
+  body: string
+  workspaceId: Id<'workspaces'>
+  channelId: Id<'channels'>
+  image?: Id<'_storage'>
+}
 
 type ChatInputProps = {
   placeholder?: string
@@ -25,8 +35,9 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const channelId = useChannelId()
 
   const { mutate: createMessage } = useCreateMessage()
+  const { mutate: generateUploadUrl } = useGenerateUploadUrl()
 
-  const handleSubmit = ({
+  const handleSubmit = async ({
     body,
     image,
   }: {
@@ -35,17 +46,38 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
   }) => {
     try {
       setIsPending(true)
+      innerRef.current?.enable(false)
 
-      createMessage(
-        {
-          body,
-          workspaceId,
-          channelId,
-        },
-        {
-          throwError: true,
-        },
-      )
+      const values: CreateMessageValues = {
+        body,
+        workspaceId,
+        channelId,
+        image: undefined,
+      }
+
+      if (image) {
+        const url = await generateUploadUrl({ throwError: true })
+
+        if (!url) {
+          throw new Error('Failed to generate upload url.')
+        }
+
+        const result = await fetch(url, {
+          method: 'POST',
+          headers: { 'Content-type': image.type },
+          body: image,
+        })
+
+        if (!result.ok) {
+          throw new Error('Failed to upload image.')
+        }
+
+        const { storageId } = await result.json()
+
+        values.image = storageId
+      }
+
+      await createMessage(values, { throwError: true })
 
       setEditKey((prevKey) => prevKey + 1)
     } catch {
