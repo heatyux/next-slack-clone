adcc1d34d9165e2cd0a17a7486ee9021097f413d
heatyux <421743974@qq.com>
2025-08-21 12:18:28 +0800

    feat: create message api implemented

---
+++ convex/messages.ts
@@ -0,0 +1,55 @@
+import { getAuthUserId } from '@convex-dev/auth/server'
+import { v } from 'convex/values'
+
+import type { Id } from './_generated/dataModel'
+import { type QueryCtx, mutation } from './_generated/server'
+
+const getMember = async (
+  ctx: QueryCtx,
+  workspaceId: Id<'workspaces'>,
+  userId: Id<'users'>,
+) => {
+  return await ctx.db
+    .query('members')
+    .withIndex('by_workspace_id_user_id', (q) =>
+      q.eq('workspaceId', workspaceId).eq('userId', userId),
+    )
+    .unique()
+}
+
+export const create = mutation({
+  args: {
+    body: v.string(),
+    image: v.optional(v.id('_storage')),
+    workspaceId: v.id('workspaces'),
+    channelId: v.optional(v.id('channels')),
+    // TODO: add conversation id
+    parentMessageId: v.optional(v.id('messages')),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx)
+
+    if (!userId) {
+      throw new Error('Unauthorized.')
+    }
+
+    const member = await getMember(ctx, args.workspaceId, userId)
+
+    if (!member) {
+      throw new Error('Unauthorized.')
+    }
+
+    // TODO: handle conversation id
+
+    const messageId = await ctx.db.insert('messages', {
+      memberId: member._id,
+      body: args.body,
+      image: args.image,
+      workspaceId: args.workspaceId,
+      channelId: args.channelId,
+      parentMessageId: args.parentMessageId,
+    })
+
+    return messageId
+  },
+})
